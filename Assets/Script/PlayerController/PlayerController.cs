585f117385122c45d4805a89053dd0b9be77f094
EdgarEscutia <157111659+EdgarEscutia@users.noreply.github.com>
2024-10-07 13:56:13 +0200

    creación de vectores
    playerController

--- Assets/Script/PlayerController/PlayerController.cs
+++ Assets/Script/PlayerController/PlayerController.cs
@@ -1,9 +1,31 @@
+using Cinemachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Input Actions")]
+    [SerializeField] InputActionReference jump;
+
+    [SerializeField] float playerjumpHeight;
+    [SerializeField] float playerSpeed;
+
+    private Vector3 playerVelocity;
+    private bool groundedPlayer;
+    private float gravityValue;
+    
+
+
+    Rigidbody rb;
+
+    [SerializeField] CinemachineVirtualCamera cam;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
     // Start is called before the first frame update
     void Start()
     {
