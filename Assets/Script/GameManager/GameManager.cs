d1e2afab630888c86398764721cc9d285b88cff2
EdgarEscutia <edgarescutiaperal@gmail.com>
2024-10-09 21:19:19 +0200

    Reset acabado
    Con la suma de muertes

--- Assets/Script/GameManager/GameManager.cs
+++ Assets/Script/GameManager/GameManager.cs
@@ -1,20 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
 
     public PlayerController playerController;
     public float playerAcceleration;
-    // Update is called once per frame
+
+    public TMP_Text scoreDead;
+    private int deathCount = 1;
+
+    void Start()
+    {
+        //scoreDead = GetComponent<TMP_Text>();
+        
+    }
+
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.R))
         {
             playerController.Respawn();
             playerController.AccelerationNull();
+            scoreDead.text = $" ATTEMPT  {deathCount}";
+            Debug.Log(deathCount);
+            deathCount++;
+            
+
         }
     }
 }
