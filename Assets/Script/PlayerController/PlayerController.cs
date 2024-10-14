3c21e24c4fe80e5a25e82816a12f8434d801f0b2
Edgar Escutia Peral <edgarescutiaperal@gmail.com>
2024-10-14 12:03:20 +0200

    creación particles

--- Assets/Script/PlayerController/PlayerController.cs
+++ Assets/Script/PlayerController/PlayerController.cs
@@ -9,19 +9,21 @@ using static TMPro.SpriteAssetUtilities.TexturePacker_JsonArray;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Mesures")]
+
     public GameManager gameManager;
 
-    [Header("Mesures")]
     public float moveSpeed = 5.0f;
-    public Vector3 moveVector;
     public float playerAcceleration = 0.0f;
     public float jumpSpeed;
-    bool salta = false;
 
+    bool salta = false;
+    bool isGrounded;
+
+    public Vector3 moveVector;
     public Vector3 respawnPos;
 
     Rigidbody rb;
-    bool isGrounded;
 
    
     void Start()
@@ -29,6 +31,36 @@ public class PlayerController : MonoBehaviour
          rb = GetComponent<Rigidbody>();
         respawnPos = transform.position;
        
+    }
+
+    void Update()
+    {
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                salta = true;
+                isGrounded = false;
+            }
+        }
+
+    }
+    private void FixedUpdate()
+    {
+        if (salta == true)
+        {
+            GetComponent<Rigidbody>().velocity += jumpSpeed * Vector3.up;
+            salta = false;
+        }
+
+
+        Vector3 moveVector = new Vector3(1, 0, 0);
+        rb.MovePosition(transform.position + moveVector * Time.deltaTime * (moveSpeed + playerAcceleration));
+        if (playerAcceleration < 5)
+        {
+            playerAcceleration = playerAcceleration + 0.01f;
+        }
+
     }
     public void AccelerationNull()
     { 
@@ -55,59 +87,16 @@ public class PlayerController : MonoBehaviour
         }
         if(other.gameObject.layer ==9)
         {
-            Debug.Log("Particle");
+            Debug.Log("Particle Red");
+        }
+        if (other.gameObject.layer == 10)
+        {
+            Debug.Log("Particle Blue");
         }
     }
 
     public void OnDestroy()
     {
         Destroy(GetComponent<Rigidbody>());
-    }
-    void Update()
-    {
-        if(isGrounded)
-        {
-            if (Input.GetKeyDown(KeyCode.Space))
-            {            
-                salta = true;
-                isGrounded = false;
-            }
-        }
-        
-            //int layerMask = 1 << 7;
-
-            //// Does the ray intersect any objects which are in layer 8?
-            ////layerMask = ~layerMask;
-            //RaycastHit hit;
-
-            //if (Physics.Raycast(transform.position, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, layerMask))
-            //{
-            //    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * hit.distance, Color.white);
-            //    Debug.Log("Did Hit");
-            //}
-            //else
-            //{
-            //    Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.white);
-            //    Debug.Log("Did not Hit");
-            //}
-        }
-    private void FixedUpdate()
-    {
-        if (salta == true)
-        {
-            GetComponent<Rigidbody>().velocity += jumpSpeed * Vector3.up;
-            salta = false;
-        }
-        
-
-        Vector3 moveVector = new Vector3(1, 0, 0);
-        rb.MovePosition(transform.position + moveVector * Time.deltaTime * (moveSpeed + playerAcceleration));
-        if(playerAcceleration < 5)
-        {
-            playerAcceleration = playerAcceleration + 0.01f;
-        }
-        
-    }
-    
-    
+    }   
 }
