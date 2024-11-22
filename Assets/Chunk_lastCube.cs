77e2baf498fa12d6085982d1d7095dd7caf75cff
Edgar Escutia Peral <edgarescutiaperal@gmail.com>
2024-11-22 09:44:02 +0100

    arreglar bugs y subir salto

--- Assets/Chunk_lastCube.cs
+++ Assets/Chunk_lastCube.cs
@@ -28,8 +28,8 @@ public class Chunk_lastCube : MonoBehaviour
 
     public void OnTriggerEnter(Collider other)
     {
-        Debug.Log("ENTRAAAAAA");
-        if (other.gameObject.layer == LayerMask.NameToLayer(layerPlayer) && !alreadyTriggered && (GameManager.instance.contador < 6 || GameManager.instance.contador < 10 && GameManager.instance.contador > 12)) //NORMAL
+        //NORMAL
+        if (other.gameObject.layer == LayerMask.NameToLayer(layerPlayer) && !alreadyTriggered && GameManager.instance.contador < 4) //NORMAL
         {
             
             float randomNumber = Random.Range(0, 1);
@@ -61,7 +61,8 @@ public class Chunk_lastCube : MonoBehaviour
             //Debug.Log(GameManager.instance.contador);
 
         }
-        else if (other.gameObject.layer == LayerMask.NameToLayer(layerPlayer) && !alreadyTriggered && GameManager.instance.contador == 6)
+        //ENTRADA PORTAL
+        else if (other.gameObject.layer == LayerMask.NameToLayer(layerPlayer) && !alreadyTriggered && GameManager.instance.contador == 4)
         {
             alreadyTriggered = true;
             GameObject randomChunk = ChunkEntradaPortal[Random.Range(0, ChunkEntradaPortal.Length)];
@@ -73,23 +74,75 @@ public class Chunk_lastCube : MonoBehaviour
             GameManager.instance.contador++;
             Debug.Log(GameManager.instance.contador);
         }
-        else if (other.gameObject.layer == LayerMask.NameToLayer(layerPlayer) && !alreadyTriggered && GameManager.instance.contador > 6 && GameManager.instance.contador < 10)
+        //ENTRE PORTALES
+        else if (other.gameObject.layer == LayerMask.NameToLayer(layerPlayer) && !alreadyTriggered && GameManager.instance.contador > 4 && GameManager.instance.contador < 7)
         {
-            //ENTRE PORTALES
-        }
 
-        else if(other.gameObject.layer == LayerMask.NameToLayer(layerPlayer) && !alreadyTriggered && GameManager.instance.contador == 10)
-        {
-            //SALIDA PORTALES
-        }
+            alreadyTriggered = true;
 
-        else if(other.gameObject.layer == LayerMask.NameToLayer(layerPlayer) && !alreadyTriggered && GameManager.instance.contador < 10 && GameManager.instance.contador > 12)
-        {
-            //NORMAL
+            randomChunk = ChunkEntrePortal[Random.Range(0, ChunkEntrePortal.Length)];
+
+            Instantiate(randomChunk, UltimoCube.transform.position, Quaternion.identity);
+
+            trigger.SetActive(false);
+
+            GameManager.instance.contador++;
         }
-        else if(other.gameObject.layer == LayerMask.NameToLayer(layerPlayer) && !alreadyTriggered && GameManager.instance.contador == 13)
+        //SALIDA PORTAL
+        else if(other.gameObject.layer == LayerMask.NameToLayer(layerPlayer) && !alreadyTriggered && GameManager.instance.contador == 7)
         {
-            //META
+            alreadyTriggered = true;
+
+            randomChunk = ChunkSalidaPortal[Random.Range(0, ChunkSalidaPortal.Length)];
+
+            Instantiate(randomChunk, UltimoCube.transform.position, Quaternion.identity);
+
+            trigger.SetActive(false);
+
+            GameManager.instance.contador++;
+        }
+        //NORMAL
+        else if(other.gameObject.layer == LayerMask.NameToLayer(layerPlayer) && !alreadyTriggered && GameManager.instance.contador > 7 && GameManager.instance.contador < 9)
+        {
+            float randomNumber = Random.Range(0, 1);
+            alreadyTriggered = true;
+
+
+
+            // % DE DIFICULTAD
+            if (randomNumber <= 0.5f)
+            {
+                randomChunk = ChunksFaciles[Random.Range(0, ChunksFaciles.Length)];
+            }
+            else if (randomNumber > 0.5f && randomNumber <= 0.7f)
+            {
+                randomChunk = ChunksMedianos[Random.Range(0, ChunksMedianos.Length)];
+            }
+            else if (randomNumber > 0.7f)
+            {
+                randomChunk = ChunksDificiles[Random.Range(0, ChunksDificiles.Length)];
+            }
+
+
+
+            Instantiate(randomChunk, UltimoCube.transform.position, Quaternion.identity);
+
+            trigger.SetActive(false);
+
+            GameManager.instance.contador++;
+        }
+        //META
+        else if(other.gameObject.layer == LayerMask.NameToLayer(layerPlayer) && !alreadyTriggered && GameManager.instance.contador == 9)
+        {
+            alreadyTriggered = true;
+
+            randomChunk = ChunkMeta[Random.Range(0, ChunkMeta.Length)];
+
+            Instantiate(randomChunk, UltimoCube.transform.position, Quaternion.identity);
+
+            trigger.SetActive(false);
+
+            GameManager.instance.contador++;
         }
     }
 }
